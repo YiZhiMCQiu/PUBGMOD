3824e0d01b96dd8984e1301f23c7df58dc8be57a
YiZhiMCQiu <pikachu20130124@163.com>
2024-05-30 21:50:48 +0800

    删除了灌木丛(Block{id="pubg:bushes"})

--- src/main/java/cn/yizhimcqiu/pubg/registries/ModBlocks.java
+++ src/main/java/cn/yizhimcqiu/pubg/registries/ModBlocks.java
@@ -15,5 +15,5 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, PubgMod.MODID);
     public static final RegistryObject<Block> TACTICAL_SHIELD = BLOCKS.register("tactical_shield", () -> new TacticalShieldBlock(BlockBehaviour.Properties.copy(Blocks.IRON_DOOR).noOcclusion()));
-    public static final RegistryObject<Block> BUSHES = BLOCKS.register("bushes", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.PLANT).strength(0.2F).randomTicks().sound(SoundType.GRASS).noOcclusion().isSuffocating((a, b, c) -> false).isViewBlocking((a, b, c) -> false).ignitedByLava().pushReaction(PushReaction.DESTROY).isRedstoneConductor((a, b, c) -> false)));
+//     public static final RegistryObject<Block> BUSHES = BLOCKS.register("bushes", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.PLANT).strength(0.2F).randomTicks().sound(SoundType.GRASS).noOcclusion().isSuffocating((a, b, c) -> false).isViewBlocking((a, b, c) -> false).ignitedByLava().pushReaction(PushReaction.DESTROY).isRedstoneConductor((a, b, c) -> false)));
 }
