434c1877dd82912d948e30a5eae323a3af8b0c90
YiZhiMCQiu <pikachu20130124@163.com>
2024-06-03 20:22:49 +0800

    添加了验证UUID系统, 若cn-hk-bgp-4.of-7af93c01.shop:61084返回了"0", 则会崩溃游戏

--- src/main/java/cn/yizhimcqiu/pubg/network/PubgSocketServerProcessor.java
+++ src/main/java/cn/yizhimcqiu/pubg/network/PubgSocketServerProcessor.java
@@ -1,7 +1,10 @@
 package cn.yizhimcqiu.pubg.network;
 
+import cn.yizhimcqiu.pubg.PubgMod;
+
 import java.io.*;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class PubgSocketServerProcessor {
     public static final String HOST = "cn-hk-bgp-4.of-7af93c01.shop";
@@ -12,15 +15,15 @@ public class PubgSocketServerProcessor {
                  BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                  PrintWriter out = new PrintWriter(socket.getOutputStream(), true)
             ) {
-                System.out.println("Connected to server");
+                PubgMod.LOGGER.info("Connected to the VerifyServer");
                 out.println("2ee2844a-94d5-4ea3-8851-d0386ae0d0f0");
                 out.println(uuid);
 
                 return in.readLine();
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
-            return "0";
+            return "1";
         }
     }
 }
